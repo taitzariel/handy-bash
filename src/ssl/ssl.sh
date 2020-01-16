913bd53512140149ae5fa00d7d46cb480fd48712
Ariel Taitz <taitzariel@gmail.com>
2020-01-16 17:25:57 +0200

    wait for stunnel to initialize

--- src/ssl/ssl.sh
+++ src/ssl/ssl.sh
@@ -10,12 +10,20 @@ sslencrypt() {
   listen_port=`find_free_port`
 
   log "forwarding localhost:$listen_port to ssl endpoint ${ssl_host}:${ssl_port}"
-  local stunnelfile=/tmp/stunnelconf
-  stunnelconf $listen_port $ssl_host $ssl_port > $stunnelfile
-  stunnel $stunnelfile 2>&1 &
+  local logfile=/tmp/stunnel.log
+  local conf=/tmp/stunnelconf
+  stunnelconf $listen_port $ssl_host $ssl_port $logfile> $conf
+  stunnel $conf 2>&1 &
 
   local pid=`echo $!`
   cleanuptrap $pid
+ 
+  log "waiting for stunnel to initialize..."
+  until grep 'Cron thread initialized' ${logfile} >/dev/null
+  do
+    sleep .5
+  done
+  log "stunnel ready"
 
   local port=$listen_port #cmd should contain $port
   verboserun "$local_connect_cmd"
@@ -25,13 +33,14 @@ stunnelconf() {
   local listen_port=$1
   local ssl_host=$2
   local ssl_port=$3
+  local logfile=$4
   cat <<eof
 fips = no
 pid =
 debug = 7 
 delay = yes
 options = NO_SSLv3
-output = /tmp/stunnel.log
+output = ${logfile}
 log = overwrite
 foreground = quiet
 [ssl-cli]
