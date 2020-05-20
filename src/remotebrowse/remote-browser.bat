a097a3b56f4c4c2e5a18a800921c4b94e482ac69
Ariel Taitz <taitzariel@gmail.com>
2020-05-20 12:25:06 +0300

    append to logfile

--- src/remotebrowse/remote-browser.bat
+++ src/remotebrowse/remote-browser.bat
@@ -2,7 +2,7 @@ set host=192.168.56.101
 set port=1234
 set logfile="C:\tmp\browse.log"
 
-echo starting %*> %logfile%
+echo starting %*>> %logfile%
 rem  "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -osint -url "%1">> %logfile%
 echo %1 | ncat %host% %port%>> %logfile%
 rem timeout /t 20
