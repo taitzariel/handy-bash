c69901ab6a7a8f8314c5e32f14226b807924e4e1
Ariel Taitz <taitza@amazon.com>
2023-10-15 10:34:12 +0300

    battery - check whether charging

--- battery/battery_alert.sh
+++ battery/battery_alert.sh
@@ -3,13 +3,13 @@
 
 while true
 do
- battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
- if [ $battery_level -le 40 ]
- then
- notify-send "Begin charging" "Battery level is ${battery_level}%!"
- elif [ $battery_level -ge 80 ]
- then
- notify-send "Unplug charger" "Battery level is ${battery_level}%!"
- fi
- sleep 60
+    battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
+    acpi -b | grep charging
+    is_charging="$?"  # 0 discharging, 1 charging
+    if [ $is_charging == 0 ] && [ $battery_level -le 40 ]; then
+        notify-send "Begin charging" "Battery level is ${battery_level}%!"
+    elif [ $is_charging == 1 ] && [ $battery_level -ge 80 ]; then
+        notify-send "Unplug charger" "Battery level is ${battery_level}%!"
+    fi
+    sleep 60
 done
